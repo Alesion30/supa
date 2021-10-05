ff04b3871e8b64003337e79f36046cc50e0929ba
Yuta Ohira <alesion90@gmail.com>
2021-10-06 07:34:38 +0900

    add: stack boltを導入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-const run = (): void => {
-  console.log("hello, world");
-  console.log("env value:", process.env.TEST);
+import { App } from "@slack/bolt";
+import { PORT } from "./config";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.message("hello-test", async ({ message, say }) => {
+  console.log("hello-test");
+  console.log("message", message);
+  say("Hello World!");
+});
+
+const run = async () => {
+  await app.start(PORT);
+  console.log(`⚡️ Bolt app is running! > http://localhost:${PORT}`);
 };
 run();
