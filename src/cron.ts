c5ec3061be5d6f409e5ecd40c954d2f7bb0e61ce
Yuta Ohira <alesion90@gmail.com>
2021-10-09 12:07:16 +0900

    add: 定時になったらメッセージを送る処理を実装

--- src/cron.ts
+++ src/cron.ts
@@ -1,6 +1,23 @@
 import { schedule } from "node-cron";
+import { app } from "./plugins/bolt";
+import { getDocs, query, where } from "./plugins/firebase";
+import { userCollectionRef } from "./schemas/user";
 
 /** cron処理を登録 */
 export const registerCron = () => {
-  schedule("* * * * *", () => console.log("hello"));
+  schedule("15 12 * * *", async () => {
+    const queryRef = query(
+      userCollectionRef,
+      where("isSubscribed", "==", true)
+    );
+    const querySnapshot = await getDocs(queryRef);
+    const docs = querySnapshot.docs;
+    docs.forEach((doc) => {
+      const user = doc.data();
+      app.client.chat.postMessage({
+        channel: user.user_id,
+        text: "テストメッセージ",
+      });
+    });
+  });
 };
